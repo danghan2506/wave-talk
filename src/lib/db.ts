5758cf3cddec617934c61243317c2df7d9cbe8f3
danghan2506 <danghan1213@gmail.com>
2025-11-07 22:10:59 +0700

    config| config database connection, initial profile when user's logged in

---
+++ src/lib/db.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import {withAccelerate} from "@prisma/extension-accelerate"
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate())
+if(process.env.NODE_ENV !== "production") globalThis.prisma = db
